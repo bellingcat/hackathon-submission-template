dd5875843517e8c38c2504eb684ddd755d088d27
Ioana Preoteasa <ioanacpreoteasa@gmail.com>
2022-09-24 13:52:22 +0100

    add 2ndary menu for analysis mode

--- network_analysis_gui.py
+++ network_analysis_gui.py
@@ -1,3 +1,4 @@
+from distutils.dir_util import copy_tree
 from email.policy import default
 import json
 import os
@@ -8,7 +9,7 @@ from datetime import datetime
 import PySimpleGUI as sg
 import re
 
-import multi_scrape as TANV
+import scrape as TANV
 
 global name_of_tool
 name_of_tool = 'TANV (Working title)'
@@ -26,9 +27,54 @@ def main():
 
     return
 
+def generate_secondary_window_analysis(project_name_:str= f'{name_of_tool} project', **kwargs):
+    return sg.Window(f'{name_of_tool} - Analysis Mode', 
+                    generate_secondary_window_analysis_layout(project_name_,**kwargs))
+
 def generate_intro_window(project_name_:str = f'{name_of_tool} project', **kwargs):
     return sg.Window(name_of_tool, 
                     generate_intro_window_layout(project_name_, **kwargs))
+
+def generate_secondary_window_analysis_layout(project_name_ = f'{name_of_tool} project', **kwargs) -> list:
+    """Fn declares and returns a list of lists where each sub-list is a row in the PySG Intro GUI menu.
+    Returns:
+        list: menu layout
+
+    ``` Title:
+
+    Options:
+    1. Enter the name of your analysis project
+    2. Enter filepath to your user info file
+    3. Enter filepath to your edge info file
+    5. Run Analysis
+
+    ```
+
+    """    
+
+    font_tuple = (menu_font, menu_txt_font_size)
+    intro_row = [sg.Text(f"""To rerun analysis on previous data, follow the steps below:
+    """, 
+                font=font_tuple )]
+                    
+    # 1. Enter project name;
+    # 2. Select the user information file;
+    # 3. Select the edge list file;
+    # 4. Click on 'RUN ANALYSIS'.
+
+
+    close_button_row = [sg.Button('Close', key='CLOSE'),]
+
+
+    layout = [intro_row,
+            get_simple_str_input_w_key("1. Enter the name of your analysis project (for your own record-keeping purposes)", '-PROJECT-NAME-', project_name_, font_tuple, **kwargs),
+            get_file_browse_input_w_key('2. Select the user information file:', '-USER-INFO-FILE-', font_tuple),
+            get_file_browse_input_w_key('3. Select the edge list file:', '-EDGE-LIST-FILE-', font_tuple),
+            [*get_simple_button_w_key("RUN ANALYSIS", "-RUN-ANALYSIS-"), *get_simple_button_w_key('Go back', '-BACK-TO-MAIN-'), *close_button_row],
+                    ]
+
+    return layout
+
 
 def generate_intro_window_layout(project_name_ = f'{name_of_tool} project', **kwargs) -> list:
     """Fn declares and returns a list of lists where each sub-list is a row in the PySG Intro GUI menu.
@@ -51,7 +97,11 @@ def generate_intro_window_layout(project_name_ = f'{name_of_tool} project', **kw
     """    
 
     font_tuple = (menu_font, menu_txt_font_size)
-    intro_row = [sg.Text(f"""Welcome to the {name_of_tool} Tool!\nTo get data and analysis for a single query, go to section A.\nTo do so for multiple queries, go to section B.\n""", 
+    intro_row = [sg.Text(f"""Welcome to the {name_of_tool} Tool!
+    To get data and analysis for a single query, go to section A.
+    To do so for multiple queries, go to section B.
+    If you wish to re-run analysis on data you've stored locally, 
+    please click on 'RERUN ANALYSIS' to open separate menu.""", 
                 font=font_tuple )]
 
 
@@ -68,8 +118,8 @@ def generate_intro_window_layout(project_name_ = f'{name_of_tool} project', **kw
             get_simple_str_display('If you want to perform multiple searches, select an Excel/CSV file.\nIt *must* have following columns present:\n\nSEARCH_TERM, MAX_DEPTH, MAX_NUMBER_TWEETS\n',), 
             get_file_browse_input_w_key('Select your queries file:', '-PARAMS-FILE-', font_tuple),
             [sg.HorizontalSeparator(key='sep')],
-            get_simple_button_w_key("RUN TOOL", "-RUN-APP-"),
-            close_button_row,
+            [*get_simple_button_w_key("RUN TOOL", "-RUN-APP-"), *get_simple_button_w_key('RERUN ANALYSIS', '-RERUN-ANALYSIS-'), *close_button_row],
+            # close_button_row,
                     ]
     return layout   
 
@@ -204,14 +254,37 @@ def run_gui():
                         continue
                     else:
                         # if no error returned then now do the network analysis
+                        print("Running single query ...")
                         run_network_analysis(**values)
 
             # NOTE: here I am just adding suggestions for other things to implement
             # e.g. if a user wants to run the analysis component again on data that was already stored,
             # especially if a user has multiple data files unmerged in a location
-            # elif event=='RERUN-ANALYSIS':
-            #     rerun_analysis_on_data_stored_locally(**values)
+            elif event=='-RERUN-ANALYSIS-':
+                
+                current_window.close()
+                #open the rerun analysis menu
+                current_window = generate_secondary_window_analysis(project_name_)
+                # event, values = current_window.read()
+                continue
 
+
+            elif event=='':
+                #trigger events for rerunning the analysis component of scrape
+                # get the user file
+
+                #get the edge list
+
+                #pass to analysis function
+
+                # rerun_analysis_on_data_stored_locally(**values)
+                continue
+
+            # event for going back to main
+            elif event=='-BACK-TO-MAIN-':
+                current_window.close()
+                current_window = generate_intro_window(project_name_)
+                continue
 
             # final event-trigger for closing GUI
             elif event=='Close' or event=='CLOSE' or event==sg.WIN_CLOSED:
@@ -380,9 +453,10 @@ def run_network_analysis(**kwargs):
     main_user = kwargs['-TWITTER-SEARCH-USER-']
     max_rec_dep = int(kwargs['-MAX-REC-DEPTH-'])
     max_n_tweets = int(kwargs['-MAX-N-TWEETS-'])
-
+    
     project_name = process_filename(kwargs['-PROJECT-NAME-'])
-
+    
+    print(f'Search: {main_user}; Depth: {max_rec_dep}, Max tweets: {max_n_tweets}')
     df = TANV.main(main_user, max_rec_dep, max_n_tweets, project_name)
 
     return df
